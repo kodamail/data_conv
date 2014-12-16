616b1abbd4d52282b1468efbd95e93e8531712a3
Chihiro Kodama <kodamac@jamstec.go.jp>
2014-12-16 14:40:51 +0900

    modify sample_usr/cnf_link.sh to avoid misleading

--- sample_usr/cnf_link.sh
+++ sample_usr/cnf_link.sh
@@ -21,28 +21,33 @@ INPUT_DIR_CTL_LIST[$i]=../../../data_1st/output/ctl_nc/02560x01280.zorg.torg
 INPUT_ML_LIST[$i]=ml_zlev
 let i++
 
-
-#INPUT_DIR_CTL_LIST[$i]=../../data_2nd/output/ctl_nc/00144x00072.zorg.torg
+#INPUT_DIR_CTL_LIST[$i]=../../../data_1st/output/ctl_nc/00144x00072.zorg.torg
+#INPUT_DIR_CTL_LIST[$i]=../../../data_2nd/output/ctl_nc/00144x00072.zorg.torg
 #INPUT_ML_LIST[$i]=ml_zlev
 #let i++
 
-#INPUT_DIR_CTL_LIST[$i]=../../data_2nd/output/ctl_nc/00288x00145.zorg.torg
+#INPUT_DIR_CTL_LIST[$i]=../../../data_1st/output/ctl_nc/00288x00145.zorg.torg
+#INPUT_DIR_CTL_LIST[$i]=../../../data_2nd/output/ctl_nc/00288x00145.zorg.torg
 #INPUT_ML_LIST[$i]=ml_zlev
 #let i++
 
-#INPUT_DIR_CTL_LIST[$i]=../../data_2nd/output/ctl_nc/00360x00181.zorg.torg
+#INPUT_DIR_CTL_LIST[$i]=../../../data_1st/output/ctl_nc/00360x00181.zorg.torg
+#INPUT_DIR_CTL_LIST[$i]=../../../data_2nd/output/ctl_nc/00360x00181.zorg.torg
 #INPUT_ML_LIST[$i]=ml_zlev
 #let i++
 
-#INPUT_DIR_CTL_LIST[$i]=../../data_2nd/output/ctl_nc/00144x00072.p37.torg
+#INPUT_DIR_CTL_LIST[$i]=../../../data_1st/output/ctl_nc/00144x00072.p37.torg
+#INPUT_DIR_CTL_LIST[$i]=../../../data_2nd/output/ctl_nc/00144x00072.p37.torg
 #INPUT_ML_LIST[$i]=ml_plev
 #let i++
 
-#INPUT_DIR_CTL_LIST[$i]=../../data_2nd/output/ctl_nc/00288x00145.p37.torg
+#INPUT_DIR_CTL_LIST[$i]=../../../data_1st/output/ctl_nc/00288x00145.p37.torg
+#INPUT_DIR_CTL_LIST[$i]=../../../data_2nd/output/ctl_nc/00288x00145.p37.torg
 #INPUT_ML_LIST[$i]=ml_plev
 #let i++
 
-#INPUT_DIR_CTL_LIST[$i]=../../data_2nd/output/ctl_nc/00360x00181.p26.torg
+#INPUT_DIR_CTL_LIST[$i]=../../../data_1st/output/ctl_nc/00360x00181.p26.torg
+#INPUT_DIR_CTL_LIST[$i]=../../../data_2nd/output/ctl_nc/00360x00181.p26.torg
 #INPUT_ML_LIST[$i]=ml_plev
 #let i++
 
