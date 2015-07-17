dff6efb9e36c297814c40ff600ab12fae657ba8e
Chihiro Kodama <kodamac@jamstec.go.jp>
2015-07-18 05:49:09 +0900

    bug fix in sample_user/cnf_link.sh
    EXT should be an array.

--- sample_usr/cnf_link.sh
+++ sample_usr/cnf_link.sh
@@ -8,7 +8,7 @@ USE_OLD=1
 # directry of control files
 #
 #EXT=grd
-EXT=nc
+#EXT=nc
 
 #INPUT_DIR_CTL=../../data_1st/ctl_nc
 
@@ -19,36 +19,43 @@ INPUT_DIR_CTL_LIST=() ; INPUT_ML_LIST=()
 
 INPUT_DIR_CTL_LIST[$i]=../../../data_1st/output/ctl_nc/02560x01280.zorg.torg
 INPUT_ML_LIST[$i]=ml_zlev
+EXT_LIST[$i]=nc
 let i++
 
 #INPUT_DIR_CTL_LIST[$i]=../../../data_1st/output/ctl_nc/00144x00072.zorg.torg
 #INPUT_DIR_CTL_LIST[$i]=../../../data_2nd/output/ctl_nc/00144x00072.zorg.torg
 #INPUT_ML_LIST[$i]=ml_zlev
+#EXT_LIST[$i]=nc
 #let i++
 
 #INPUT_DIR_CTL_LIST[$i]=../../../data_1st/output/ctl_nc/00288x00145.zorg.torg
 #INPUT_DIR_CTL_LIST[$i]=../../../data_2nd/output/ctl_nc/00288x00145.zorg.torg
 #INPUT_ML_LIST[$i]=ml_zlev
+#EXT_LIST[$i]=nc
 #let i++
 
 #INPUT_DIR_CTL_LIST[$i]=../../../data_1st/output/ctl_nc/00360x00181.zorg.torg
 #INPUT_DIR_CTL_LIST[$i]=../../../data_2nd/output/ctl_nc/00360x00181.zorg.torg
 #INPUT_ML_LIST[$i]=ml_zlev
+#EXT_LIST[$i]=nc
 #let i++
 
 #INPUT_DIR_CTL_LIST[$i]=../../../data_1st/output/ctl_nc/00144x00072.p37.torg
 #INPUT_DIR_CTL_LIST[$i]=../../../data_2nd/output/ctl_nc/00144x00072.p37.torg
 #INPUT_ML_LIST[$i]=ml_plev
+#EXT_LIST[$i]=nc
 #let i++
 
 #INPUT_DIR_CTL_LIST[$i]=../../../data_1st/output/ctl_nc/00288x00145.p37.torg
 #INPUT_DIR_CTL_LIST[$i]=../../../data_2nd/output/ctl_nc/00288x00145.p37.torg
 #INPUT_ML_LIST[$i]=ml_plev
+#EXT_LIST[$i]=nc
 #let i++
 
 #INPUT_DIR_CTL_LIST[$i]=../../../data_1st/output/ctl_nc/00360x00181.p26.torg
 #INPUT_DIR_CTL_LIST[$i]=../../../data_2nd/output/ctl_nc/00360x00181.p26.torg
 #INPUT_ML_LIST[$i]=ml_plev
+#EXT_LIST[$i]=nc
 #let i++
 
 #----------------------------------------#
