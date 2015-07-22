0bd8915a0ba9f310a29ebd191c259f7db4682704
Chihiro Kodama <kodamac@jamstec.go.jp>
2015-07-23 05:06:30 +0900

    modify sample_usr/common.sh

--- sample_usr/common.sh
+++ sample_usr/common.sh
@@ -1,18 +1,32 @@
-export LD_LIBRARY_PATH=/home/kodama/opt/lib:${LD_LIBRARY_PATH}
-export LIBRARY_PATH=/home/kodama/opt/lib:${LIBRARY_PATH}
-export INCLUDE=/home/kodama/opt/include:${INCLUDE}
+#
+# edit below for your environment.
+#
+
+# necessary external library
+export PATH=/cwork5/kodama/program/sh_lib/bash_common/release-20150719:${PATH}
+export PATH=/cwork5/kodama/program/sh_lib/grads_ctl/release-20150717:${PATH}
+export PATH=/cwork5/kodama/program/sh_lib/diff-path/0.01r3:${PATH}
 
 
-
-XDEF_NAT=2560
-YDEF_NAT=1280
-ZDEF_NAT=61
-ZDEF_ISCCP=49
-
-
-DIR_NICAM=/bwork3/kodama/NICAM_src/NICAM_20140929/bin
+DIR_NICAM=/cwork5/kodama/NICAM_src/NICAM_20150109/NICAM/bin
 BIN_NC2CTL=${DIR_NICAM}/nc2ctl
 BIN_ROUGHEN=${DIR_NICAM}/roughen
 BIN_Z2PRE=${DIR_NICAM}/z2pre
 
-BIN_ZONAL_MEAN=zonal_mean-0.03r1
+#BIN_ZONAL_MEAN=zonal_mean-0.03r1
+
+BIN_DIFF_PATH=diff-path
+BIN_ZONAL_MEAN=/cwork5/kodama/program/for/zonal_mean/0.03r1/zonal_mean
+
+
+#export LD_LIBRARY_PATH=/home/kodama/opt/lib:${LD_LIBRARY_PATH}
+#export LIBRARY_PATH=/home/kodama/opt/lib:${LIBRARY_PATH}
+#export INCLUDE=/home/kodama/opt/include:${INCLUDE}
+
+#
+# Native (i.e. finest mesh) grid data information
+#
+XDEF_NAT=2560
+YDEF_NAT=1280
+ZDEF_NAT=38
+ZDEF_ISCCP=49
