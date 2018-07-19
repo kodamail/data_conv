fbef8fc42c241a3e676344c3611236bc7d5dc964
Chihiro Kodama <kodamac@jamstec.go.jp>
2018-07-19 11:49:31 +0900

    support multiple CNFID

--- make.sh
+++ make.sh
@@ -3,11 +3,11 @@
 # usage: ./make.sh [ OVERWRITE=rm ] [CNFID] job-1 [ job-2 [ job-3 ... ] ]
 #
 JOB_LIST=( )
+CNFID_LIST=( )
 OPT=""
-CNFID=def
 while [ -n "$1" ] ; do
     if [ -f cnf/$1.sh ] ; then
-	CNFID=$1
+	CNFID_LIST=( ${CNFID_LIST[@]} $1 )
     elif [ -f $1 ] ; then
 	JOB_LIST=( ${JOB_LIST[@]} $1 )
     else
@@ -16,12 +16,16 @@ while [ -n "$1" ] ; do
     shift
 done
 
+if [ ${#CNFID_LIST[@]} -eq 0 ] ; then
+    CNFID_LIST=( def )
+fi
 if [ ${#JOB_LIST[@]} -eq 0 ] ; then
     echo "usage:"
     echo "$0 [ OVERWRITE=rm ] [CNFID] job-1 [ job-2 [ job-3 ... ] ]"
     exit
 fi
 
+for CNFID in ${CNFID_LIST[@]} ; do
 for JOB in ${JOB_LIST[@]} ; do
     [ "${JOB}" != "${JOB%common.sh}" -o "${JOB}" != "${JOB%sh~}" ] && continue
     
@@ -41,6 +45,7 @@ for JOB in ${JOB_LIST[@]} ; do
     echo "#"
     echo "#======================================#"
     echo ""
+done
 done
 
 echo "$0 normally finished."
