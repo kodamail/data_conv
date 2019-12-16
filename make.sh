f3ac040e00d45f72e3901514ea56863aa907bbb6
Chihiro Kodama <kodamac@jamstec.go.jp>
2019-12-16 11:12:04 +0900

    support -l in make.sh

--- make.sh
+++ make.sh
@@ -5,6 +5,7 @@
 JOB_LIST=( )
 CNFID_LIST=( )
 OPT=""
+FLAG_LINK=off
 while [[ -n "$1" ]] ; do
     if [[ -f cnf/$1.sh ]] ; then
 	CNFID_LIST+=( $1 )
@@ -12,19 +13,27 @@ while [[ -n "$1" ]] ; do
 	CNFID_LIST+=( ${BASH_REMATCH[2]} )
     elif [[ -f $1 ]] ; then
 	JOB_LIST+=( $1 )
+    elif [[ $1 = "-l" ]] ; then
+	FLAG_LINK=on
     else
 	OPT="${OPT} $1"
     fi
     shift
 done
 
-if [[ ${#CNFID_LIST[@]} -eq 0 ]] ; then
+if (( ${#CNFID_LIST[@]} == 0 )) ; then
     CNFID_LIST=( def )
 fi
-if [[ ${#JOB_LIST[@]} -eq 0 ]] ; then
+if (( ${#JOB_LIST[@]} == 0 )) ; then
     echo "usage:"
-    echo "$0 [ OVERWRITE=rm ] [ CNFID-1 [ CNFID-2 ... ] ] job-1 [ job-2 ... ]"
+    echo "$0 [-l] [ OVERWRITE=rm ] [ CNFID-1 [ CNFID-2 ... ] ] job-1 [ job-2 ... ]"
     exit
+fi
+
+if [[ ${FLAG_LINK} = "on" ]] ; then
+    for CNFID in ${CNFID_LIST[@]} ; do
+	./link.sh cnf/${CNFID}.sh || exit 1
+    done
 fi
 
 for CNFID in ${CNFID_LIST[@]} ; do
